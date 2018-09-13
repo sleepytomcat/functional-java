472ec451149b16140b554dfafba90332b1eb5306
Sergei <sleepytomcat@gmail.com>
2018-09-12 21:43:03 -0700

    chapter03.exercise04

---
+++ src/main/java/chapter03/exercise04.java
@@ -0,0 +1,68 @@
+package chapter03;
+
+import java.util.*;
+
+public class exercise04 {
+	public static void main(String... args) {
+		List<Integer> list = list(1,2,3,4,5);
+		Integer head = head(list);
+		List<Integer> tail = tail(list);
+
+		List<Integer> emptyList = list();
+
+		try {
+			Integer headOfEmptyList = head(emptyList);
+		}
+		catch (Throwable ex) {
+			System.out.println(ex.getMessage());
+		}
+
+		try {
+			List<Integer> tailOfEmptyList = tail(emptyList);
+		}
+		catch (Throwable ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+
+	public static <T> List<T> list() {
+		return Collections.emptyList();
+	}
+
+	public static <T> List<T> list(T value) {
+		return Collections.singletonList(value);
+	}
+
+	public static <T> List<T> list(Collection<T> elements) {
+		return Collections.unmodifiableList(new ArrayList<>(elements));
+	}
+
+	@SafeVarargs
+	public static <T> List<T> list(T... elements) {
+		return Collections.unmodifiableList(
+				Arrays.asList(
+						Arrays.copyOf(
+								elements, elements.length
+						)
+				)
+		);
+	}
+
+	public static <T> T head(List<T> list) {
+		if (list.size() >= 1)
+			return list.get(0);
+		else
+			throw new IllegalStateException("head of empty list");
+	}
+
+	public static <T> List<T> tail(List<T> src) {
+		if (src.size() == 0)
+			throw new IllegalStateException("tail of empty list");
+		else
+			return exercise04.<T>copy(src.subList(1, src.size()));
+	}
+
+	private static <T> List<T> copy(List<T> src) {
+		return new ArrayList<T>(src);
+	}
+}
