d7ffc4a8650c1a016a17738a244f7e0412bd8914
Sergei <sleepytomcat@gmail.com>
2018-09-13 22:54:06 -0700

    chapter03.exercise07

---
+++ src/main/java/chapter03/exercise07.java
@@ -0,0 +1,50 @@
+package chapter03;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Function;
+
+public class exercise07 {
+	public static void main(String... args) {
+		List<Integer> list = list(1,2,3,4,5);
+		System.out.println(exercise07.<Integer, String>foldRight("0", list, x -> y -> addIS(x, y)));
+	}
+
+	@SafeVarargs
+	public static <T> List<T> list(T... elements) {
+		return Collections.unmodifiableList(Arrays.asList(Arrays.copyOf(elements, elements.length)));
+	}
+
+	public static <T> T head(List<T> list) {
+		if (list.size() >= 1)
+			return list.get(0);
+		else
+			throw new IllegalStateException("head of empty list");
+	}
+
+	public static <T> List<T> tail(List<T> src) {
+		if (src.size() == 0)
+			throw new IllegalStateException("tail of empty list");
+		else
+			return exercise07.<T>copy(src.subList(1, src.size()));
+	}
+
+	private static <T> List<T> copy(List<T> src) {
+		return new ArrayList<T>(src);
+	}
+
+	static String addIS(String s, Integer i) {
+		return "(" + i + " + " + s + ")";
+	}
+
+	public static <T,U> U foldRight(U identityElement, List<T> list, final Function<U, Function<T, U>> foldingFunction) {
+		if (list.size() == 0)
+			return identityElement;
+		else {
+			U tailFolded = foldRight(identityElement, tail(list), foldingFunction);
+			return foldingFunction.apply(tailFolded).apply(head(list));
+		}
+	}
+}
