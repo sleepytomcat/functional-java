99c1b7aa2c6d6988e66a09be1ec1d2ccbbb025f5
Sergei <sleepytomcat@gmail.com>
2018-09-16 22:56:32 -0700

    typeo fix chapter04.exercise01

--- src/main/java/chapter04/exercise01.java
+++ src/main/java/chapter04/exercise01.java
@@ -4,7 +4,7 @@ public class exercise01 {
 	public static void main(String... args) {
 		System.out.println("Starting...");
 		System.out.println(fibonacci(50));
-		System.out.println(fibonacciTailTesursion(50, 1, 1));
+		System.out.println(fibonacciTailResursion(50, 1, 1));
 	}
 
 	static int fibonacci(int number) {
@@ -14,12 +14,12 @@ public class exercise01 {
 			return fibonacci(number - 1) + fibonacci(number - 2);
 	}
 
-	static int fibonacciTailTesursion(int number, int accumulatorA, int accumulatorB) {
+	static int fibonacciTailResursion(int number, int accumulatorA, int accumulatorB) {
 		if (number == 0)
 			return accumulatorB;
 		else if (number == 1)
 			return accumulatorA;
 		else
-			return fibonacciTailTesursion(number-1, accumulatorA + accumulatorB, accumulatorA);
+			return fibonacciTailResursion(number-1, accumulatorA + accumulatorB, accumulatorA);
 	}
 }
