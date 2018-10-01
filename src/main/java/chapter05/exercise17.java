077eb042e73f5541e9a40fe694e81a9f331ee59a
Sergei <sleepytomcat@gmail.com>
2018-09-30 23:27:28 -0700

    chapter05.exercise19,20

---
+++ src/main/java/chapter05/exercise17.java
@@ -0,0 +1,121 @@
+package chapter05;
+
+import java.util.function.Function;
+
+public class exercise17 {
+	public static void main(String... args) {
+		List<Integer> list = List.list(1, 3, 5);
+		System.out.println(map(list, x -> x * 3));
+	}
+
+	static abstract class List<T> {
+		public abstract T head();
+		public abstract List<T> tail();
+		public abstract boolean isEmpty();
+
+		public static final List NIL = new Nil();
+		private List() {}
+		private static class Nil<T> extends List<T> {
+			public T head() {
+				throw new IllegalStateException("head() called on empty list");
+			}
+
+			public List<T> tail() {
+				throw new IllegalStateException("tail() called on empty list");
+			}
+
+			public boolean isEmpty() {
+				return true;
+			}
+
+			@Override
+			public String toString() {
+				return "[NIL]";
+			}
+		}
+
+		public static class Cons<T> extends List<T> {
+			private Cons(T head, List<T> tail) {
+				this.head = head;
+				this.tail = tail;
+			}
+
+			public T head() {
+				return head;
+			}
+
+			public List<T> tail() {
+				return tail;
+			}
+
+			public boolean isEmpty() {
+				return false;
+			}
+
+			private T head;
+			private List<T> tail;
+
+			@Override
+			public String toString() {
+				return toString("", this).eval();
+			}
+
+			private static <T> TailCall<String> toString(String accumulator, List<T> list) {
+				if (list.isEmpty())
+					return TailCall.ret("[" + accumulator + " NIL]");
+				else
+					return TailCall.sus(() -> toString(accumulator + list.head() + ", ", list.tail()));
+			}
+		}
+
+		@SuppressWarnings("unchecked")
+		static <T> List<T> list() {
+			return NIL;
+		}
+
+		@SafeVarargs
+		static <T> List<T> list(T ... a) {
+			List<T> n = list();
+			for (int i = a.length - 1; i >= 0; i--) {
+				n = new Cons<>(a[i], n);
+			}
+			return n;
+		}
+
+		public static <T> List<T> reverse(List<T> list) {
+			return foldLeft(list, List.list(), head -> accumulator -> new Cons<>(head, accumulator));
+		}
+	}
+
+	public static <U, V> V foldLeft(List<U> list, V identity, Function<U, Function<V, V>> folding) {
+		return foldLeft_(list, identity, folding).eval();
+	}
+
+	private static <U, V> TailCall<V> foldLeft_(List<U> list, V accumulator, Function<U, Function<V, V>> folding) {
+		return list.isEmpty()
+				? TailCall.ret(accumulator)
+				: TailCall.sus(() -> foldLeft_(list.tail(), folding.apply(list.head()).apply(accumulator), folding));
+	}
+
+	public static <U, V> V foldRight(List<U> list, V identity, Function<U, Function<V, V>> folding) {
+		return foldRight_(List.reverse(list), identity, folding).eval();
+	}
+
+	private static <U, V> TailCall<V> foldRight_(List<U> list, V accumulator, Function<U, Function<V, V>> folding) {
+		return list.isEmpty()
+				? TailCall.ret(accumulator)
+				: TailCall.sus(() -> foldRight_(list.tail(), folding.apply(list.head()).apply(accumulator), folding));
+	}
+
+	public static <T> List<T> concat(List<T> first, List<T> second) {
+		return foldRight(first, second, secondHead -> accumulatedFirst -> new List.Cons<>(secondHead, accumulatedFirst));
+	}
+
+	public static <T> List<T> flatten(List<List<T>> listOfLists) {
+		return foldLeft(listOfLists, List.list(), x -> y -> concat(y, x));
+	}
+
+	public static <U,V> List<V> map(List<U> list, Function<U,V> mapper) {
+		return foldRight(list, List.list(), x -> y -> new List.Cons<>(mapper.apply(x), y));
+	}
+}
